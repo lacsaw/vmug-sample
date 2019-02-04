ba5bf2be31c0406613870f23f76bd6d3109b7972
Linux <lacsaw@hotmail.com>
2019-02-05 10:01:36 +1100

    change message

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	log.Println("Starting helloworld application...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello que pasa universe!\n")
+		fmt.Fprintf(w, "Hello que pasa??? mundo!\n")
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
